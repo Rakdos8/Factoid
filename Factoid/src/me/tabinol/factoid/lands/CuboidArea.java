b405455ee9d344979355166d0d19daa2278e2989
tabinol <tabinol@4712614b-fb95-4817-b30c-960f6ab9129e>
2013-08-03 23:24:11 +0000

    Collisions, Arealist in Land

--- Factoid/src/me/tabinol/factoid/lands/CuboidArea.java
+++ Factoid/src/me/tabinol/factoid/lands/CuboidArea.java
@@ -1,38 +1,42 @@
 package me.tabinol.factoid.lands;
 
+import me.tabinol.factoid.utilities.Calculate;
+
 public class CuboidArea {
-    
+
     String worldName;
     private int x1, y1, z1, x2, y2, z2;
-    
+
     public CuboidArea(String worldName, int x1, int y1, int z1, int x2, int y2, int z2) {
-        
+
         this.worldName = worldName;
-        this.x1 = x1;
-        this.y1 = y1;
-        this.z1 = z1;
-        this.x2 = x2;
-        this.y2 = y2;
-        this.z2 = z2;
+        this.x1 = Calculate.lowerInt(x1, x2);
+        this.x2 = Calculate.greaterInt(x1, x2);
+        this.y1 = Calculate.lowerInt(y1, y2);
+        this.y2 = Calculate.greaterInt(y1, y2);
+        this.z1 = Calculate.lowerInt(z1, z2);
+        this.z2 = Calculate.greaterInt(z1, z2);
     }
-    
+
     public boolean equals(CuboidArea area2) {
-        
+
         return worldName.equalsIgnoreCase(area2.worldName)
                 && x1 == area2.x1 && y1 == area2.y1 && z1 == area2.z1
                 && x2 == area2.x2 && y2 == area2.y2 && z2 == area2.z2;
     }
-    
+
     public CuboidArea copyOf() {
-        
+
         return new CuboidArea(worldName, x1, y1, z1, x2, y2, z2);
     }
-    
+
     public boolean isCollision(CuboidArea area2) {
-        
-        // ********* TO DO ****************
-        
-        return false;
+
+        return ((Calculate.isInInterval(x1, area2.x1, area2.x2)
+                || Calculate.isInInterval(area2.x1, x1, x2)))
+                && ((Calculate.isInInterval(y1, area2.y1, area2.y2)
+                || Calculate.isInInterval(area2.y1, y1, y2)))
+                && ((Calculate.isInInterval(z1, area2.z1, area2.z2)
+                || Calculate.isInInterval(area2.z1, z1, z2)));
     }
-    
 }
--- Factoid/src/me/tabinol/factoid/lands/Land.java
+++ Factoid/src/me/tabinol/factoid/lands/Land.java
@@ -1,27 +1,48 @@
 package me.tabinol.factoid.lands;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Land {
     
     private String name;
-    private HashSet<CuboidArea> areas;
+    private HashMap<Integer, CuboidArea> areas;
+    int nextAreaId = 0;
     
     public Land(String landName, CuboidArea area) {
         
-        HashSet<CuboidArea> genAreas = new HashSet<>();
-        genAreas.add(area);
+        HashMap<Integer, CuboidArea> genAreas = new HashMap<>();
+        genAreas.put(nextAreaId ++, area);
         createLand(landName, genAreas);
     }
     
-    public Land(String landName, HashSet<CuboidArea>areas) {
+    public Land(String landName, HashMap<Integer, CuboidArea>areas) {
         
         createLand(landName, areas);
     }
     
-    private void createLand(String landName, HashSet<CuboidArea>areas) {
+    private void createLand(String landName, HashMap<Integer, CuboidArea>areas) {
         
         name = landName;
         this.areas = areas;
+    }
+    
+    public int addArea(CuboidArea area) {
+        
+        int areaId = nextAreaId ++;
+        
+        areas.put(areaId, area);
+        
+        return areaId;
+    }
+    
+    public boolean removeArea(int areaId) {
+        
+        if(areas.containsKey(areaId)) {
+            areas.remove(areaId);
+            return true;
+        }
+        
+        return false;
     }
 }
