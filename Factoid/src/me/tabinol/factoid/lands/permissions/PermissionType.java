f174e37930a4e8fb6c9d2220ff2ffff9d2e4f8d9
tabinol <tabinol@4712614b-fb95-4817-b30c-960f6ab9129e>
2013-10-09 20:48:14 +0000

    Aller rechercher le type de permission par le String

--- Factoid/src/me/tabinol/factoid/lands/permissions/PermissionType.java
+++ Factoid/src/me/tabinol/factoid/lands/permissions/PermissionType.java
@@ -3,41 +3,59 @@ package me.tabinol.factoid.lands.permissions;
 
 public enum PermissionType {
     
-    UNDEFINED(0),
-    BUILD_PLACE(1),
-    BUILD_DESTROY(2),
-    DROP(3),
-    USE_CRAFT(4),
-    USE_BREW(5),
-    USE_SMELT(6),
-    USE_CHEST(7),
-    USE_DOOR(8),
-    USE_SWITCH(9),
-    USE_PRESSUREPLATE(10),
-    USE_TRAPPEDCHEST(11),
-    USE_STRING(12),
-    ANIMAL_KILL(13),
-    ANIMAL_CUT(14),
-    ANIMAL_FEED(15),
-    ANIMAL_ACCOUPLE(16),
-    MOB_KILL(17),
-    MOB_HEAL(18),
-    VILLAGER_KILL(19),
-    VILLAGER_GOLEM_KILL(20),
-    BUCKET_WATER(21),
-    BUCKET_LAVA(22),
-    FIRE(23),
-    TNT(24),
-    ENTER(25),
-    REMOVE(26),
-    KICK(27),
-    BAN(28),
-    WHOS(29);
+    UNDEFINED(0,"UNDEFINED"),
+    BUILD_PLACE(1,"BUILD_PLACE"),
+    BUILD_DESTROY(2,"BUILD_DESTROY"),
+    DROP(3,"DROP"),
+    USE_CRAFT(4,"USE_CRAFT"),
+    USE_BREW(5,"USE_BREW"),
+    USE_SMELT(6,"USE_SMELT"),
+    USE_CHEST(7,"USE_CHEST"),
+    USE_DOOR(8,"USE_DOOR"),
+    USE_SWITCH(9,"USE_SWITCH"),
+    USE_PRESSUREPLATE(10,"USE_PRESSUREPLATE"),
+    USE_TRAPPEDCHEST(11,"USE_TRAPPEDCHEST"),
+    USE_STRING(12,"USE_STRING"),
+    ANIMAL_KILL(13,"ANIMAL_KILL"),
+    ANIMAL_CUT(14,"ANIMAL_CUT"),
+    ANIMAL_FEED(15,"ANIMAL_FEED"),
+    ANIMAL_ACCOUPLE(16,"ANIMAL_ACCOUPLE"),
+    MOB_KILL(17,"MOB_KILL"),
+    MOB_HEAL(18,"MOB_HEAL"),
+    VILLAGER_KILL(19,"VILLAGER_KILL"),
+    VILLAGER_GOLEM_KILL(20,"VILLAGER_GOLEM_KILL"),
+    BUCKET_WATER(21,"BUCKET_WATER"),
+    BUCKET_LAVA(22,"BUCKET_LAVA"),
+    FIRE(23,"FIRE"),
+    TNT(24,"TNT"),
+    ENTER(25,"ENTER"),
+    REMOVE(26,"REMOVE"),
+    KICK(27,"KICK"),
+    BAN(28,"BAN"),
+    WHOS(29,"WHOS");
     
     private final int value;
+    private final String permissionName;
     
-    private PermissionType(final int value) {
+    private PermissionType(final int value, final String permissionName) {
         
         this.value = value;
+        this.permissionName = permissionName;
+    }
+    
+    @Override
+    public String toString() {
+        
+        return permissionName;
+    }
+    
+    public PermissionType getFromString(String permName) {
+        
+        for(PermissionType pt : values()) {
+            if(pt.toString().equals(permName)) {
+                return pt;
+            }
+        }
+        return null;
     }
 }
