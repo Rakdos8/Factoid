7d8e03d783e8bce72b02ffc7b91d0ace3903af2f
tabinol <tabinol@4712614b-fb95-4817-b30c-960f6ab9129e>
2013-10-10 02:45:39 +0000

    correct flagtype

--- Factoid/src/me/tabinol/factoid/lands/flags/FlagType.java
+++ Factoid/src/me/tabinol/factoid/lands/flags/FlagType.java
@@ -3,32 +3,29 @@ package me.tabinol.factoid.lands.flags;
 public enum FlagType {
 
     //Global Flags
-    UNDIFINED(0,"UNDIFINED","Undifined"),
-    FIRESPREAD(1,"Firespread","Prevent the spread of fire."),
-    FIRE(2,"Fire","Prevent fire."),
+    UNDIFINED(0,"UNDEFINED","Undefined"),
+    FIRESPREAD(1,"FIRESPREAD","Prevent the spread of fire."),
+    FIRE(2,"FIRE","Prevent fire."),
     //Land Flags
-    BUY(100,"Buy","Can buy this Area."),
-    SELL(111,"Sell","Can sell this Area."),
-    RENT(112,"Rent","Can rent this Area."),
-    CREEPER(113,"Creeper","Prevent creeper damage to this Area."),
+    BUY(100,"BUY","Can buy this Area."),
+    SELL(111,"SELL","Can sell this Area."),
+    RENT(112,"RENT","Can rent this Area."),
+    CREEPER(113,"CREEPER","Prevent creeper damage to this Area."),
     TNT(114,"TNT","Prevent TNT damage to this Area."),
     MOB(115,"MOB","Prevent Mob spawn in this Area."),
     PVP(116,"PVP","Prevent PVP in this Area."),
-    PVPFACTION(117,"Faction PVP","Prevent Faction PVP in this Area"),
-    OWNER(118,"Owner","Owner of this Area."),
-    RESIDENT(119,"Resident","Resident of this Area."),
-    EXCLUDEBLOCK(120,"Exclude Block","Excluded block in this Area."),
-    EXCLUDEENTITIE(121,"Exclude Entitie","Excluded entity in this Area."),
-    MESSAGEJOIN(122,"Message Join","Message when player join this Area."),
-    MESSAGEQUIT(123,"Message Quit","Message when player quit this Area."),
-    PRIORITY(124,"Priority","Priority of this Area."),
-    KEEPINVENTORY(125,"Keep Inventory","If player can keep is Inventory.");
+    FACTION_PVP("FACTION_PVP","?"),
+    RESIDENTS("RESIDENTS""Resident of this Area."),
+    EXCLUDE_BLOCKS("EXCLUDE_BLOCKS","Excluded block in this Area."),
+    EXCLUDE_ENTITIES("EXCLUDE_ENTITIES","Excluded entity in this Area."),
+    MESSAGE_JOIN("MESSAGE_JOIN","Message when player join this Area."),
+    MESSAGE_QUIT("MESSAGE_QUIT","Message when player quit this Area."),
+    KEEP_INVENTORY("KEEP_INVENTORY","If player can keep is Inventory.");
     
-    private final int value;
     private final String name;
     private final String description;
 
-    private FlagType(final int value,final String name,final String description) {
+    private FlagType(final String name,final String description) {
 
         this.value = value;
         this.name = name;
---
+++ Factoid/src/me/tabinol/factoid/lands/flags/FlagTypeValueType.java
@@ -0,0 +1,7 @@
+package me.tabinol.factoid.lands.flags;
+
+public enum FlagTypeValueType {
+    
+    private 
+    
+}
