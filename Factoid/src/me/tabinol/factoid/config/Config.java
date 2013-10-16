550c8bec0690c163725426c1e673d9cb80f2c0e3
tabinol <tabinol@4712614b-fb95-4817-b30c-960f6ab9129e>
2013-10-16 02:38:16 +0000

    Use YML for default and world config

--- Factoid/src/me/tabinol/factoid/config/Config.java
+++ Factoid/src/me/tabinol/factoid/config/Config.java
@@ -1,5 +1,6 @@
 package me.tabinol.factoid.config;
 
+import java.io.File;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeMap;
@@ -11,12 +12,16 @@ import me.tabinol.factoid.lands.permissions.Permission;
 import me.tabinol.factoid.lands.permissions.PermissionType;
 import me.tabinol.factoid.playercontainer.PlayerContainer;
 import me.tabinol.factoid.playercontainer.PlayerContainerType;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public class Config {
 
     private Factoid thisPlugin;
     private FileConfiguration config;
+    private FileConfiguration landDefault;
+    private FileConfiguration worldConfig;
     // Configuration
     public boolean debug = false;
     public List<String> Worlds = null;
@@ -43,14 +48,27 @@ public class Config {
 
         thisPlugin = Factoid.getThisPlugin();
         thisPlugin.saveDefaultConfig();
+        if (!new File(thisPlugin.getDataFolder(), "landdefault.yml").exists()) {
+            thisPlugin.saveResource("landdefault.yml", false);
+        }
+        if (!new File(thisPlugin.getDataFolder(), "worldconfig.yml").exists()) {
+            thisPlugin.saveResource("worldconfig.yml", false);
+        }
         config = thisPlugin.getConfig();
-
+        loadCustom();
         getConfig();
     }
 
     public void reloadConfig() {
 
         thisPlugin.reloadConfig();
+        loadCustom();
+    }
+    
+    private void loadCustom() {
+
+        landDefault = YamlConfiguration.loadConfiguration(new File(thisPlugin.getDataFolder(), "landdefault.yml"));
+        worldConfig = YamlConfiguration.loadConfiguration(new File(thisPlugin.getDataFolder(), "worldconfig.yml"));
     }
 
     private void getConfig() {
@@ -81,40 +99,68 @@ public class Config {
     public TreeMap<String, DummyLand> getLandOutsideArea() {
 
         TreeMap<String, DummyLand> landList = new TreeMap<>();
-        Set<String> keys = config.getConfigurationSection("world").getKeys(false);
+        Set<String> keys = worldConfig.getConfigurationSection("").getKeys(false);
 
-        for(String key : keys) {
-            String worldName = key.replace("world.", "");
-            List<String> permList = config.getStringList(key + ".ContainerPermissions");
-            List<String> flagList = config.getStringList(key + ".ContainerFlags");
-            landList.put(worldName, landCreate(permList, flagList));
+        for (String key : keys) {
+            String worldName = key;
+            Factoid.getLog().write("Create conf for World: " + key);
+            landList.put(worldName, landCreate(worldConfig, key + ".ContainerPermissions",
+                    key + ".ContainerFlags"));
         }
-        
+
         return landList;
     }
-    
-    public DummyLand getLandDefaultConf() {
-        
-            List<String> permList = config.getStringList("land.DefaultConfig.ContainerPermissions");
-            List<String> flagList = config.getStringList("land.DefaultConfig.ContainerFlags");
-            
-            return landCreate(permList, flagList);
-    }
-    
-    private DummyLand landCreate(List<String> perms, List<String> flags) {
-        
-        DummyLand dl = new DummyLand();
 
-        for(String perm : perms) {
-            String[] substr = perm.split(":");
-            dl.addPermission(PlayerContainer.create(null, PlayerContainerType.getFromString(substr[0]), substr[1]), 
-                    new Permission(PermissionType.getFromString(substr[2]), Boolean.parseBoolean(substr[3]), Boolean.parseBoolean(substr[4])));
+    public DummyLand getLandDefaultConf() {
+
+        Factoid.getLog().write("Create default conf for lands");
+        return landCreate(landDefault, "ContainerPermissions", "ContainerFlags");
+    }
+
+    private DummyLand landCreate(FileConfiguration fc, String perms, String flags) {
+
+        DummyLand dl = new DummyLand();
+        ConfigurationSection csPerm = fc.getConfigurationSection(perms);
+        ConfigurationSection csFlags = fc.getConfigurationSection(flags);
+
+        // Add permissions
+        if (csPerm != null) {
+            for (String container : csPerm.getKeys(false)) {
+                if (container.equalsIgnoreCase("Faction")
+                        || container.equalsIgnoreCase("Group")
+                        || container.equalsIgnoreCase("Player")) {
+                    for (String containerName : fc.getConfigurationSection(perms + "." + container).getKeys(false)) {
+                        for (String perm : fc.getConfigurationSection(perms + "." + container + "." + containerName).getKeys(false)) {
+                            Factoid.getLog().write("Container: " + container + ":" + containerName + ", " + perm);
+                            dl.addPermission(
+                                    PlayerContainer.create(null, PlayerContainerType.getFromString(container), containerName),
+                                    new Permission(PermissionType.getFromString(perm),
+                                    fc.getBoolean(perms + "." + container + "." + containerName + "." + perm + ".Value"), 
+                                    fc.getBoolean(perms + "." + container + "." + containerName + "." + perm + ".heritable")));
+                        }
+                    }
+                } else {
+                    for (String perm : fc.getConfigurationSection(perms + "." + container).getKeys(false)) {
+                        Factoid.getLog().write("Container: " + container + ", " + perm);
+                        dl.addPermission(
+                                PlayerContainer.create(null, PlayerContainerType.getFromString(container), null),
+                                new Permission(PermissionType.getFromString(perm),
+                                fc.getBoolean(perms + "." + container + "." + perm + ".Value"), 
+                                fc.getBoolean(perms + "." + container + "." + perm + ".heritable")));
+                    }
+                }
+            }
         }
-        for(String flag : flags) {
-            String[] substr = flag.split(":");
-            dl.addFlag(new LandFlag(FlagType.getFromString(substr[0]), substr[1], Boolean.parseBoolean(substr[2])));
+
+        // add flags
+        if (csFlags != null) {
+            for (String flag : csFlags.getKeys(false)) {
+                Factoid.getLog().write("Flag: " + flag);
+                dl.addFlag(new LandFlag(FlagType.getFromString(flag),
+                        fc.getString(flags + "." + flag + ".Value"), fc.getBoolean(flags + "." + flag + ".heritable")));
+            }
         }
-        
+
         return dl;
     }
 }
